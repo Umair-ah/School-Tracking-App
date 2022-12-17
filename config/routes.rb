6dd2dbffc1f9add81e55d2b1909bd71c2a11553d
Umair Ahmed <umairahmed200203.umair@gmail.com>
2022-12-17 23:53:57 +0530

    index and show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+
+  devise_for :users, controllers: { confirmations: 'confirmations' }
   devise_scope :user do  
     get '/users/sign_out' => 'devise/sessions#destroy'     
  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   root "pages#home"
+  resources :users, only: [:index, :show]
 end
