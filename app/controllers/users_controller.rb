465279650ce452a468151c87a5c8ee541bf8f84b
Umair Ahmed <umairahmed200203.umair@gmail.com>
2022-12-21 22:22:35 +0530

    Delete method on index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,18 @@
 class UsersController < ApplicationController
 
     def index
-        @users = User.all
+        @users = User.all.order(created_at: :desc)
     end
 
     def show
         @user = User.find(params[:id])
+    end
+
+    def destroy
+        @user = User.find(params[:id])
+        @user.destroy
+        respond_to do |format|
+            format.html{ redirect_to users_path, notice: "User was successfully destroyed!" }
+        end
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,5 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   root "pages#home"
-  resources :users, only: [:index, :show]
+  resources :users, only: [:index, :show, :destroy]
 end
