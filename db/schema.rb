679654774ffbac636c6b5936ae6e2cf908660649
Umair Ahmed <umairahmed200203.umair@gmail.com>
2022-12-24 15:18:04 +0530

    User data displayed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_21_170158) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_24_094356) do
 # Could not dump table "users" because of following StandardError
 #   Unknown type 'inet' for column 'current_sign_in_ip'
 
