0b574d54be180037a17133ecdd2dc857bb2727d2
Umair Ahmed <umairahmed200203.umair@gmail.com>
2023-01-06 21:27:32 +0530

    Module Helper and ids link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+    def boolean_label(value)
+        case value
+            when true
+                content_tag(:span, value, class: "btn btn-success btn-sm disabled")
+            when false
+                content_tag(:span, value, class: "btn btn-danger btn-sm disabled")
+        end
+    end
 end
