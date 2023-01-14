cf5680cdd0306aa84831475da2ebc78cbf9040ff
Umair Ahmed <umairahmed200203.umair@gmail.com>
2023-01-14 17:10:46 +0530

    Enrollments table

---
+++ app/models/enrollment.rb
@@ -0,0 +1,9 @@
+class Enrollment < ApplicationRecord
+
+    belongs_to :user
+    belongs_to :course
+
+    validates_uniqueness_of :user_id, scope: :course_id
+    validates_uniqueness_of :course_id, scope: :user_id
+
+end
