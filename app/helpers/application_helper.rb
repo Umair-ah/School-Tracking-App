197c73210d9ff8044985cb8193f3054c4f54939b
Umair Ahmed <umairahmed200203.umair@gmail.com>
2023-05-23 23:15:22 +0530

    styled fronte end

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+    def boolean_label(value)
+        case value
+          when true
+            content_tag(:span, value, class: "badge rounded-pill text-bg-primary")
+          when false
+            content_tag(:span, value, class: "badge rounded-pill text-bg-danger")
+        end
+    end
 end
