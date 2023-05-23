a4591e39f1b39804bfa02e03db31db1ad16ffa0b
Umair Ahmed <umairahmed200203.umair@gmail.com>
2023-05-23 09:43:24 +0530

    devise lockable

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,5 +7,11 @@ class UsersController < ApplicationController
     def show
       @user = User.find(params[:id])
     end
+
+    def destroy
+      @user = User.find(params[:id])
+      @user.destroy
+      redirect_to users_path, notice: "User is successfully destroyed!"
+    end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { confirmations: 'confirmations' }
 
-  resources :users, only: [:index, :show]
+  resources :users, only: [:index, :show, :destroy]
   
   root "statics#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
