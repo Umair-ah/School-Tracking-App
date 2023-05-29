4b983ead0b9bc19fd66ffb784a8da1dc02c1b069
Umair Ahmed <umairahmed200203.umair@gmail.com>
2023-05-29 20:40:25 +0530

    edit lesson attendances with cocoon

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_27_180800) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_29_144906) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "attendances", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "lesson_id", null: false
+    t.string "status", default: "planned"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["lesson_id"], name: "index_attendances_on_lesson_id"
+    t.index ["user_id"], name: "index_attendances_on_user_id"
+  end
 
   create_table "classrooms", force: :cascade do |t|
     t.string "name"
@@ -91,6 +101,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_27_180800) do
     t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
   end
 
+  add_foreign_key "attendances", "lessons"
+  add_foreign_key "attendances", "users"
   add_foreign_key "courses", "classrooms"
   add_foreign_key "courses", "services"
   add_foreign_key "courses", "users"
