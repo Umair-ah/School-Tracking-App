dd415c14e2ead9539f3ccdfe4996dcd4a64527d3
Umair Ahmed <umairahmed200203.umair@gmail.com>
2023-06-07 06:02:54 +0530

    calculate price of all student attendances

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -2,6 +2,7 @@ class Attendance < ApplicationRecord
     belongs_to :user
     belongs_to :lesson
     validates :status, presence: true
+    monetize :student_price_start, as: :student_price_start_cents
 
     # user can not attend the same lesson twice
     validates_uniqueness_of :user_id, scope: :lesson_id
@@ -10,5 +11,9 @@ class Attendance < ApplicationRecord
     STATUSES = [:planned, :attended, :not_attended]
     def self.statuses
         STATUSES.map{ |status| [status, status] }
+    end
+
+    after_create do
+        update_column :student_price_start, lesson.course.service.client_price
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_29_144906) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_07_002345) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_29_144906) do
     t.string "status", default: "planned"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "student_price_start", default: 0, null: false
     t.index ["lesson_id"], name: "index_attendances_on_lesson_id"
     t.index ["user_id"], name: "index_attendances_on_user_id"
   end
